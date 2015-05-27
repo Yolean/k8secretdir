99f6e0aa40f02de4c34ec5465ee7753438ceb86d
Andrew Lytvynov <lytvynov.a.v@gmail.com>
2015-05-27 15:39:03 -0700

    Initial commit
    Recursively walk the path and base64-encode all files as k8s yaml
    secrets config would expect it.

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	if len(os.Args) == 1 {
+		fmt.Printf("usage: %s <direcotry path>", os.Args[0])
+		os.Exit(2)
+	}
+	path := os.Args[1]
+	if err := filepath.Walk(path, encode); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func encode(path string, fi os.FileInfo, err error) error {
+	if err != nil {
+		return err
+	}
+	if fi.IsDir() {
+		return nil
+	}
+
+	f, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	fmt.Printf("  %s: >\n    ", filepath.Base(path))
+	enc := base64.NewEncoder(base64.StdEncoding, os.Stdout)
+	defer fmt.Println()
+	defer enc.Close()
+	_, err = io.Copy(enc, f)
+	if err != nil {
+		return err
+	}
+	return nil
+}
