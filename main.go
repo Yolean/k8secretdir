44ecb731dc5fbb7681426dce310317ccc122f538
Staffan Olsson <staffan@repos.se>
2016-01-10 12:55:15 +0100

    Looks better in terminal

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	if len(os.Args) == 1 {
-		fmt.Printf("usage: %s <direcotry path>", os.Args[0])
+		fmt.Printf("usage: %s <direcotry path>\n", os.Args[0])
 		os.Exit(2)
 	}
 	path := os.Args[1]
